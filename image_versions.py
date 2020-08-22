4db79a8af89feb08c5f61a5bda2a3153f15e86ac
Wade Wells ~ Pack3tL0ss <github.com Pack3tL0ss>
2020-08-21 23:56:16 -0500

    Initial commit

--- image_versions.py
+++ image_versions.py
@@ -9,14 +9,15 @@ import datetime
 import utils
 import requests
 import argparse
-import logging
+import socket
+# import log
 # import yaml
 # from tabulate import tabulate
 # import argparse
-import os.path
+import os
 import time
-import utils
 
+path = os.path
 LOCK = threading.Lock()
 COUNT = 3
 controllers = ''
@@ -24,169 +25,220 @@ final_result = []
 port = ''
 localtime = datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
 outfile = 'results.log'
-logfile = 'image_versions.log'
+log_file = 'image_versions.log'
 yaml_config = 'config.yaml'
-
-# setting the logging level
-logging.basicConfig(filename=logfile, level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
-logger = logging.getLogger(__name__)
+DEBUG = os.environ.get('DEBUG', False)
 
 
-def connect():
-    """
-    This function is utilized to read controllers information from config file and start the Multi-threaded session
-    """
+# setting the log level
+log = utils.MyLogger(log_file, debug=DEBUG)
+log.info(f" {'-' * 10 } Script Startup {'-' * 20 }")
 
-    # -- // Get Config from config.yaml \\ --
-    config = utils.get_yaml_file(yaml_config)
+# -- // Get Config from config.yaml \\ --
+config = utils.get_yaml_file(yaml_config)
 
-    i = 0
-    thread_array = []
-    mcds = config.get('conductors', [])
-    t = [None] * len(mcds)
 
-    for mcd in mcds:
-        '''------------Initiating a thread for API session to each controller IP----------------'''
-        _this = [mcd, config['user'], config['pass'], 'MM']
-        thread_array.append(_this)
-        t[i] = threading.Thread(target=exec_api, args=(_this))
+class ManagedDevice:
+    def __init__(self, data: dict = None, connection=None):
+        if connection:
+            self.connection = connection
+        if data:
+            self.update_data(data)
 
-        t[i].daemon = True
-        t[i].start()
+    def update_data(self, data):
+        self.name = data.get('Name')
+        self.cfg_id = data.get('Config ID')
+        self.sync_time = data.get('Config Sync Time (sec)')
+        self.cfg_state = data.get('Configuration State')
+        self.ip = data.get('IP Address')
+        self.location = data.get('Location')
+        self.model = data.get('Model')
+        self.status = data.get('Status')
+        self.type = data.get('Type')
+        self.version = data.get('Version')
 
-        time.sleep(0.5)
-        i += 1
 
-    count = 0
-    for thread in t:
-        thread.join()
-        logging.info(f"Thread with ip - {thread_array[count]} completed")
-        count += 1
+class Controllers():
+    def __init__(self, conductors):
+        self.conductors = conductors
+        self.data = {}
+        self.run()
 
-def find_up_mds(data):
-    """ checks for all the UP state MDs and returns a list of MDs"""
-    val = []
-    for k in data.keys():
-        if data[k][3] == 'up':
-            val.append(k)
-    return (val)
+    def run(self):
+        # Start parallel threads to establish session with Mobility Conductors
+        # updates data attribute
+        self.start_controller_threads(self.conductors)
+        start = len(self.data)
+        if self.data:
+            self.exec_api()
+        if len(self.data) > start:
+            md_list = [dev for dev in self.data if not hasattr(self.data[dev], 'connection')]
+            self.start_controller_threads(md_list)
+            self.exec_api(conductor=False)
 
-def exec_api(ip_address, username, password, model_type=""):
-    """
-    This function will login into the controller via API and execute all the commands sequentially for all the IPs.
-    :param ip_address: IP address of the controller
-    :param username: Username of the controller
-    :param password: password of the controller
-    :param model_type: if the controller is MM/MD
-    """
+    def start_controller_threads(self, devices):
+        """Get Session for each
+        """
+        thread_array = []
+        t = [None] * len(devices)
 
-    try:
+        for i, dev in enumerate(devices):
+            '''------------Initiating a thread for API session to each controller IP----------------'''
+            _this = [dev, config['user'], config['pass']]
+            thread_array.append(_this)
+            t[i] = threading.Thread(target=self.get_session, args=(_this))
 
-        if not os.path.isfile(outfile):
-            # create result file
-            fd = open(outfile, 'w')
-            fd.close()
-        con = utils.ConnectUtil(ip_address, username, password, port)
-        if not con.api_login():
-            raise ConnectionRefusedError
+            t[i].daemon = True
+            t[i].start()
 
-        if model_type == 'MM':
-            ''' get all the MDs connect to the particular MM'''
-            con.execute_command("show switches")
-            controller_ids = con.ps.parse_show_switches()
-            md_list = find_up_mds(controller_ids)
-            ''' execute show configuration committed on all MDs'''
-            if len(md_list) > 0:
-                print('validating configuration on MDs connected to MM {}\n'.format(ip_address))
+            time.sleep(0.5)
 
-                for cntr in md_list:
-                    cntr = cntr.strip('\r\n')
-                    print(cntr)
-                    # cmd = 'show configuration committed ' + cntr
-                    # con.execute_command(cmd)
-                    # ''' parse the tunnel configuration tunnel source and tunnel destination'''
-                    # tun_conf = con.ps.parse_show_config_tunnel(cntr)
+        for idx, thread in enumerate(t):
+            thread.join()
+            log.info(f"Thread with ip - {thread_array[idx][0]} completed", show=True)
 
-                    # ''' validate for overlapping src/dst IP'''
-                    # result1 = validate_tunnel_ip(tun_conf)
-                    # # final_result.append(result1)
-                    # '''parse the tunnel group configuration from show config'''
-                    # tun_grp = con.ps.parse_tunnel_group(cntr)
-                    # '''validate tunnel grp assigned to the tunnels on the node'''
-                    # result2 = validate_tunnel_grp(tun_conf, tun_grp)
-                    # #  final_result.append(result2)
-                    # ''' write results to the result file'''
-                    # fd = open(outfile, "a")
-                    # fd.write('\noverlapping ip tunnel validation result for node {} connected to MM {},\n'
-                    #          .format(cntr, ip_address))
-                    # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
-                    # if (len(result1) > 1):
-                    #     fd.write(tabulate(result1, headers="firstrow"))
-                    #     fd.write('\n')
-                    # else:
-                    #     fd.write('No tunnels found for node {}'.format(cntr))
-                    #     fd.write('\n')
+    def get_session(self, dev, username, password):
+        try:
+            ip = socket.gethostbyname(dev)
+            con = utils.AosConnect(ip, user=username, password=password)
+            r = con.api_login()
+            if r.ok:
+                if ip not in self.data:
+                    self.data[ip] = ManagedDevice(connection=con)
+                else:
+                    setattr(self.data[ip], 'connection',con)
+            else:
+                log.error(r.err)
+                raise r.error.__name__
+        except socket.gaierror:
+            log.critical(f"[{dev}] Unable to resolve host.")
+        except ConnectionRefusedError as e:
+            log.critical(f"[{dev}] Unable to connect to Controller. Login Failed.\n{e}")
+        except requests.RequestException as e:
+            log.critical(f"[{dev}] Requests Exception {e}")
+        except Exception as e:
+            log.critical(f"[{dev}] Exception Occured {e}")
 
-                    # fd.write(
-                    #     '\ntunnel group validation result for node {} connected to MM {} ,\n'.format(cntr, ip_address))
-                    # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
+    def exec_api(self, conductor=True):
+        """
+        This function will login into the controller via API and execute all the commands sequentially for all the IPs.
+        :param ip_address: IP address of the controller
+        :param username: Username of the controller
+        :param password: password of the controller
+        :param model_type: if the controller is MM/MD
+        """
+        if conductor:
+            ''' get all the MDs connect to the Mobility Conductor'''
+            for dev in self.data.copy():
+                if self.data[dev].connection.handle:
+                    con = self.data[dev].connection
+                    res = con.execute_command("show switches")
+                    if res.ok:
+                        switch_dict = utils.parse_show_switches(res.json)
+                        for ip in switch_dict:
+                            self.data[ip] = ManagedDevice(data=switch_dict[ip])
+        else:
+            for dev in self.data:
+                if hasattr(self.data[dev], "connection"):
+                    con = self.data[dev].connection
+                    res = con.execute_command("show image version")
+                    if res.ok:
+                        img_dict = utils.parse_show_image_version(res.json)
+                        for k, v in img_dict.items():
+                            setattr(self.data[dev], k, v)
 
-                    # if (len(result2) > 1):
-                    #     fd.write(tabulate(result2, headers='firstrow'))
-                    #     fd.write('\n')
-                    # else:
-                    #     fd.write('No tunnels found for node {}'.format(cntr))
-                    #     fd.write('\n')
+        print(self.data)
 
-                    # fd.close()
 
-                    '''print results on the terminal'''
-                    # if verbose and len(result1) > 1 and len(result2) > 1:
-                    #     print('overlapping ip tunnel validation result for node {} connected to MM {},\n'
-                    #           .format(cntr, ip_address))
-                    #     print(tabulate(result1, headers='firstrow', tablefmt="fancy_grid"))
-                    #     print('\ntunnel group validation result for node {} connected to MM {} ,\n'.format(cntr,
-                    #                                                                                        ip_address))
-                    #     print(tabulate(result2, headers='firstrow', tablefmt="fancy_grid"))
+            # con.execute_command("show switches")
+            # md_dict = con.ps.parse_show_switches()
 
-        if model_type == 'MD':
-            ## check tunnel status
-            print('validating tunnel status on MD {}\n'.format(ip_address))
-            cmd = 'show ip interface brief'
-            # con.execute_command(cmd)
+            # # md_list = find_up_mds(controller_ids)
+            # ''' execute show configuration committed on all MDs'''
+            # if md_dict:
+            #     print('validating configuration on MDs connected to MM {}\n'.format(ip_address))
 
-            # '''parse tunnel UP / down status from each of the MD'''
-            # intf_state = con.ps.parse_show_ip_interface(ip_address)
+            #     for md in md_dict:
+            #         print(md)
 
-            # ''' validate if tunnel status is UP or DOWN'''
-            # result3 = validate_tunnel_status(intf_state)
-            # fd = open(outfile, "a")
-            # '''write to file the results'''
-            # fd.write('\ntunnel status validation result for MD node {},\n'.format(ip_address))
-            # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
-            # if len(result3) == 1:
-            #     logging.critical('NO Tunnels Found for MD node {}'.format(ip_address))
-            #     fd.write('No Tunnels found for node {}\n'.format(ip_address))
-            #     if verbose:
-            #         print('No Tunnels found for node {}.\n'.format(ip_address))
-            # else:
-            #     ''' write results to the result file'''
-            #     fd.write(tabulate(result3, headers="firstrow"))
-            #     final_result.append(result3)
-            #     fd.write('\n')
-            #     '''print results on the terminal'''
-            #     if verbose:
-            #         print('\ntunnel status validation result for MD node {},\n'.format(ip_address))
-            #         print(tabulate(result3, headers="firstrow", tablefmt="fancy_grid"))
-            # fd.close()
-        # con.handle.close()
-    except ConnectionRefusedError as e:
-        logging.critical("Unable to connect to Controller. Login Failed. {}".format(ip_address))
-    except requests.RequestException as e:
-        logging.critical("New Connection error. Network Failure {}".format(ip_address))
-    except Exception as e:
-        logging.critical("session timeout/ controller crashed {}".format(ip_address))
+                        # tun_conf = con.ps.parse_show_config_tunnel(cntr)
+
+                        # ''' validate for overlapping src/dst IP'''
+                        # result1 = validate_tunnel_ip(tun_conf)
+                        # # final_result.append(result1)
+                        # '''parse the tunnel group configuration from show config'''
+                        # tun_grp = con.ps.parse_tunnel_group(cntr)
+                        # '''validate tunnel grp assigned to the tunnels on the node'''
+                        # result2 = validate_tunnel_grp(tun_conf, tun_grp)
+                        # #  final_result.append(result2)
+                        # ''' write results to the result file'''
+                        # fd = open(outfile, "a")
+                        # fd.write('\noverlapping ip tunnel validation result for node {} connected to MM {},\n'
+                        #          .format(cntr, ip_address))
+                        # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
+                        # if (len(result1) > 1):
+                        #     fd.write(tabulate(result1, headers="firstrow"))
+                        #     fd.write('\n')
+                        # else:
+                        #     fd.write('No tunnels found for node {}'.format(cntr))
+                        #     fd.write('\n')
+
+                        # fd.write(
+                        #     '\ntunnel group validation result for node {} connected to MM {} ,\n'.format(cntr, ip_address))
+                        # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
+
+                        # if (len(result2) > 1):
+                        #     fd.write(tabulate(result2, headers='firstrow'))
+                        #     fd.write('\n')
+                        # else:
+                        #     fd.write('No tunnels found for node {}'.format(cntr))
+                        #     fd.write('\n')
+
+                        # fd.close()
+
+                        # '''print results on the terminal'''
+                        # if verbose and len(result1) > 1 and len(result2) > 1:
+                        #     print('overlapping ip tunnel validation result for node {} connected to MM {},\n'
+                        #           .format(cntr, ip_address))
+                        #     print(tabulate(result1, headers='firstrow', tablefmt="fancy_grid"))
+                        #     print('\ntunnel group validation result for node {} connected to MM {} ,\n'.format(cntr,
+                        #                                                                                        ip_address))
+                        #     print(tabulate(result2, headers='firstrow', tablefmt="fancy_grid"))
+
+            # if model_type == 'MD':
+            #     ## check tunnel status
+            #     print('validating tunnel status on MD {}\n'.format(ip_address))
+            #     cmd = 'show ip interface brief'
+                # con.execute_command(cmd)
+
+                # '''parse tunnel UP / down status from each of the MD'''
+                # intf_state = con.ps.parse_show_ip_interface(ip_address)
+
+                # ''' validate if tunnel status is UP or DOWN'''
+                # result3 = validate_tunnel_status(intf_state)
+                # fd = open(outfile, "a")
+                # '''write to file the results'''
+                # fd.write('\ntunnel status validation result for MD node {},\n'.format(ip_address))
+                # fd.write('+++++++++++++++++++++++++++++++++++++++++++++++++\n')
+                # if len(result3) == 1:
+                #     log.critical('NO Tunnels Found for MD node {}'.format(ip_address))
+                #     fd.write('No Tunnels found for node {}\n'.format(ip_address))
+                #     if verbose:
+                #         print('No Tunnels found for node {}.\n'.format(ip_address))
+                # else:
+                #     ''' write results to the result file'''
+                #     fd.write(tabulate(result3, headers="firstrow"))
+                #     final_result.append(result3)
+                #     fd.write('\n')
+                #     '''print results on the terminal'''
+                #     if verbose:
+                #         print('\ntunnel status validation result for MD node {},\n'.format(ip_address))
+                #         print(tabulate(result3, headers="firstrow", tablefmt="fancy_grid"))
+                # fd.close()
+            # con.handle.close()
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Run Tunnel Validation  tool.\n'
@@ -209,4 +261,9 @@ if __name__ == "__main__":
 
     port = args.port
     verbose = args.verbose
-    connect()
+
+    mcds = config.get('conductors', [])
+    if mcds:
+        Controllers(mcds)
+    else:
+        print('No Data, Check config.yaml')
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 # portions of this script based on the work already done by https://github.com/aruba/arubaos8-example-scripts
 
 
+import logging
 import re
 import requests
 import urllib3
@@ -22,7 +23,97 @@ headers = {
 }
 
 
-class ConnectUtil:
+class Response():
+    def __init__(self, ok: bool, output=None, error=None, status_code=None, state=None, do_json=False,  **kwargs):
+        self.ok = ok
+        self.text = output
+        self.error = error
+        self.state = state
+        self.status_code = status_code
+        if 'json' in kwargs:
+            self.json = kwargs['json']
+        else:
+            self.json = None if not do_json else json.dumps(output)
+
+
+class MyLogger:
+    def __init__(self, log_file, debug=False):
+        self.log_msgs = []
+        self.DEBUG = debug
+        self.verbose = False
+        self.log_file = log_file
+        self._log = self.get_logger()
+        self.name = self._log.name
+
+    def get_logger(self):
+        '''Return custom log object.'''
+        fmtStr = "%(asctime)s [%(process)d][%(levelname)s]: %(message)s"
+        dateStr = "%m/%d/%Y %I:%M:%S %p"
+        logging.basicConfig(filename=self.log_file,
+                            level=logging.DEBUG if self.DEBUG else logging.INFO,
+                            format=fmtStr,
+                            datefmt=dateStr)
+        return logging.getLogger('ConsolePi')
+
+    def log_print(self, msgs, log=False, show=True, level='info', *args, **kwargs):
+        msgs = [msgs] if not isinstance(msgs, list) else msgs
+        _msgs = []
+        _logged = []
+        for i in msgs:
+            if log and i not in _logged:
+                getattr(self._log, level)(i)
+                _logged.append(i)
+            if '\n' in i:
+                _msgs += i.replace('\t', '').replace('\r', '').split('\n')
+            elif i.startswith('[') and ']' in i:
+                _msgs.append(i.split(']', 1)[1].replace('\t', '').replace('\r', ''))
+            else:
+                _msgs.append(i.replace('\t', '').replace('\r', '').strip())
+
+        msgs = []
+        [msgs.append(i) for i in _msgs
+            if i and i not in msgs and i not in self.log_msgs]
+
+        if show:
+            self.log_msgs += msgs
+            for m in self.log_msgs:
+                print(m)
+            self.log_msgs = []
+
+    def show(self, msgs, log=False, show=True, *args, **kwargs):
+        self.log_print(msgs, show=show, log=log, *args, **kwargs)
+
+    def debug(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='debug', *args, **kwargs)
+
+    # -- more verbose debugging - primarily to get json dumps
+    def debugv(self, msgs, log=True, show=False, *args, **kwargs):
+        if self.DEBUG and self.verbose:
+            self.log_print(msgs, log=log, show=show, level='debug', *args, **kwargs)
+
+    def info(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, *args, **kwargs)
+
+    def warning(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='warning', *args, **kwargs)
+
+    def error(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='error', *args, **kwargs)
+
+    def exception(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='exception', *args, **kwargs)
+
+    def critical(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='critical', *args, **kwargs)
+
+    def fatal(self, msgs, log=True, show=False, *args, **kwargs):
+        self.log_print(msgs, log=log, show=show, level='fatal', *args, **kwargs)
+
+    def setLevel(self, level):
+        getattr(self._log, 'setLevel')(level)
+
+
+class AosConnect(Response):
 
     def __init__(self, ip, user='', password='', port=4343):
         self.ip = ip
@@ -30,7 +121,6 @@ class ConnectUtil:
         self.user = user
         self.password = password
         self.handle = None
-        self.ps = ParseCommand()
         self.output = ''
 
     def api_login(self) -> object:
@@ -42,16 +132,15 @@ class ConnectUtil:
         url = f"https://{self.ip}:{self.port}/v1/api/login"
         payload = {'username': self.user, 'password': self.password}
 
-        try:
-            if self.ip:
-                r = requests.post(url, data=payload, verify=False)
+        if self.ip:
+            try:
+                r = requests.post(url, data=payload, headers=headers, verify=False)
                 self.handle = r
-                print(r.text)
-                return True
-            else:
-                raise Exception("No IP")
-        except Exception as err:
-            return False
+                return Response(ok=True, output=r.text, json=r.json(), status_code=r.status_code)
+            except Exception as err:
+                return Response(ok=False, error=err, status_code=r.status_code)
+        else:
+            return Response(ok=False, error="No IP address")
 
     def execute_command(self, cmd: str) -> object:
         """
@@ -59,36 +148,58 @@ class ConnectUtil:
         :param cmd: command to be executed on device
         :return: data containing output of the command
         """
-        parameters = {"UIDARUBA": self.handle.headers['Set-Cookie'].split(';')[0].split('=')[1], "command": cmd}
-        self.handle = requests.get(f"https://{self.ip}:{self.port}/v1/configuration/showcommand",
-                                   verify=False,
-                                   headers=headers,
-                                   params=parameters, cookies=self.handle.cookies)
-        data = self.handle.content.decode('utf-8')
-        self.ps.value = data
-        self.output = data
+        try:
+            parameters = {"UIDARUBA": self.handle.headers['Set-Cookie'].split(';')[0].split('=')[1], "command": cmd}
+            r = requests.get(f"https://{self.ip}:{self.port}/v1/configuration/showcommand",
+                             verify=False,
+                             headers=headers,
+                             params=parameters, cookies=self.handle.cookies
+                             )
+            if r.ok:
+                self.handle = r
+                return Response(ok=True, output=r.text, status_code=r.status_code, json=r.json())
+            else:
+                return Response(ok=False, error=r.reason, status_code=r.status_code)
+        except Exception as err:
+            return Response(ok=False, error=err)
 
 
-class ParseCommand:
+def parse_show_switches(data: dict):
+    """
+    Parses show switches output
+    :returns dictionary of ManagedDevice objects by switch IP
+    """
+    # data = json.loads(data)
+    switch_dict = {}
+    for dev in data.get('All Switches', {}):
+        switch_dict[dev['IP Address']] = dev
 
-    def __init__(self, value='', sc_model=''):
-        self.value = value
-        self.sc_model = sc_model
+    return switch_dict
 
-    def parse_show_switches(self):
-        """
-        Parses show switches output
-        :returns dictionary with Node Name , IP address, Configuration Status and Status of MD
-        """
-        localtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        data = json.loads(self.value)
-        new_values = {}
-        for var in data['All Switches']:
-            if var['Type'] == 'MD':
-                attribute_name = var['Name']
-                tupple = (localtime, var['IP Address'], var['Configuration State'], var['Status'])
-                new_values.update({attribute_name.rstrip(): tupple})
-        return new_values
+
+def parse_show_image_version(data):
+    """Parse return from show image version command
+    """
+    # data = json.loads(data)
+    data = data.get('_data', [])[0].split('\n')
+    img_dict = {}
+    _part = 'err'
+    for line in data:
+        if line.startswith("Partition"):
+            if '0:0' in line:
+                _part = '0:0'
+            elif '0:1' in line:
+                _part = '0:1'
+            if 'Default boot' in line:
+                img_dict['default_boot'] = f"0:{line.split(':')[2].replace(' **Default boot**', '')}"
+            continue
+        if line.startswith('Software Version'):
+            img_dict[f"version {_part}"] = line.split(':')[1].strip()
+        if line.startswith('Build num'):
+            img_dict[f"version {_part}"] += f" Build: {line.split(':')[1].strip()}"
+
+    return img_dict
+
 
 def get_yaml_file(yaml_file):
     '''Return dict from yaml file.'''
